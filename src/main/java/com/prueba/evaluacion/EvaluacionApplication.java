f864e11d06349e09df6ee2d69f4ca7f35a2f905d
juand <>
2022-10-13 00:37:27 +0200

    console application config

--- src/main/java/com/prueba/evaluacion/EvaluacionApplication.java
+++ src/main/java/com/prueba/evaluacion/EvaluacionApplication.java
@@ -1,13 +1,26 @@
 package com.prueba.evaluacion;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EvaluacionApplication {
+public class EvaluacionApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvaluacionApplication.class, args);
+			
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if (args.length > 0) {
+			for (String string : args) {
+				System.out.println(string);
+			}
+		}
+		
 	}
 
 }
